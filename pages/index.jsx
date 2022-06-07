238fab67710e0605c08d3ae1584e798e55a984ff
josehfortes <jose@seja.dev>
2022-06-07 15:58:18 -0300

    Criado onclick

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,21 @@
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
 
 export default function HomePage() {
+  const handleLabelClick = () => {
+    console.log('HANDLE LABEL CLICK')
+  }
+
   return (
     <>
-      <Title>Titulo da página</Title>
-      <Button>Botão</Button>
+      <Title onClick={handleLabelClick}>Formulário de login</Title>
+      <label onClick={handleLabelClick}>Voltar a página...</label>
+      <form>
+        <Input type="email" placeholder="Digite seu e-mail" />
+        <Input type="password" placeholder="Digite sua senha" />
+        <Button onClick={handleLabelClick}>Botão</Button>
+      </form>
       <label>LABEL TESTE</label>
       <button>Button</button>
     </>
