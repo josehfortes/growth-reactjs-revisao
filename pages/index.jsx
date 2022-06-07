5c7467e88fc9c4369518e574b19246d94da58377
josehfortes <jose@seja.dev>
2022-06-07 17:18:21 -0300

    Criado onChange e onSubmit

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,15 +7,26 @@ export default function HomePage() {
     console.log('HANDLE LABEL CLICK')
   }
 
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+    console.log('HANDLE SUBMIT FORM')
+  }
+
+  let email = ''
+  const handleChangeInput = (event) => {
+    email = event.target.value
+  }
+
   return (
     <>
       <Title onClick={handleLabelClick}>Formulário de login</Title>
       <label onClick={handleLabelClick}>Voltar a página...</label>
-      <form>
-        <Input type="email" placeholder="Digite seu e-mail" />
-        <Input type="password" placeholder="Digite sua senha" />
-        <Button onClick={handleLabelClick}>Botão</Button>
+      <form onSubmit={handleSubmitForm}>
+        <Input type="text" placeholder="Digite seu e-mail" onChange={handleChangeInput} />
+        <p>O e-mail digitado foi: {email}</p>
+        <Button type="submit">Botão dentro do form</Button>
       </form>
+      <Button onClick={handleLabelClick}>Botão FORA do form</Button>
       <label>LABEL TESTE</label>
       <button>Button</button>
     </>
